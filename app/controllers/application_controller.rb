ef52151e7f7638912b16340d703451863f08968c
Dylan Menard <148174649+Dylanm22@users.noreply.github.com>
2024-03-13 20:24:59 +0000

    added pundit and policy for users and photos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 class ApplicationController < ActionController::Base
+ include Pundit
+  
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+  private
 
+   def user_not_authorized
+     flash[:alert] = "you are not authorized to preform this action"
+
+     redirect_back fallback_location: root_url
+    end
+
+  
   protected
 
   def configure_permitted_parameters
